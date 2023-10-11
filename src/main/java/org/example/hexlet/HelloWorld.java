10130129dbc18f56dc111b257bc09ae96c03e8c2
stanmoskalenko <st.moskalenko@gmail.com>
2023-10-11 15:39:49 +0300

    force

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,2 +1,15 @@
-package org.example.hexlet;public class HelloWorld {
+package org.example.hexlet;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Создаем приложение
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        // Описываем, что загрузится по адресу /
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.start(7070); // Стартуем веб-сервер
+    }
 }
